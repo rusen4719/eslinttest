96548b8577ed89714b954fcef8bcefc05fb8fe2d
jryong125 <jryong125@naver.com>
2022-08-31 16:20:43 +0900

    commit with error final

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log('test lint error2');
+  console.log('test lint error3');
   return (
     <div className="App">
       <header className="App-header">
