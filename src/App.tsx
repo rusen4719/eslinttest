d1d631e34c1e4347aba7968ad8c0b1eca6574085
jryong125 <jryong125@naver.com>
2022-08-31 16:22:39 +0900

    test no error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log('test lint error3');
   return (
     <div className="App">
       <header className="App-header">
